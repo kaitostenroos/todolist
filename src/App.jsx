d50a00e0f7d8d32ba9e68170680ce464cec3a391
unknown <kaito.stenroos@outlook.com>
2024-10-07 22:07:37 +0300

    Tabs added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
 import TodoList from './TodoList.jsx';
+import { Box, Tab} from '@mui/material';
+import { TabContext, TabList, TabPanel } from '@mui/lab';
+import { useState} from 'react';
 import './App.css'
 
 
 function App() {
+  const [value, setValue] = useState('1')
+  const handleChange = (event, newValue) => {
+    setValue(newValue)
+  };
 
   return (
-    <>
-     <TodoList />
-    </>
+    <Box>
+      <TabContext value={value}>
+        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+          <TabList aria-label='Tabs' onChange={handleChange}>
+            <Tab label='Home' value='1'/>
+            <Tab label='Todos' value='2'/>
+          </TabList>
+        </Box>
+        <TabPanel value='1'>Welcome</TabPanel>
+        <TabPanel value='2'>{<TodoList />}</TabPanel>
+      </TabContext>
+      
+    </Box>
+     
   );
 }
 
